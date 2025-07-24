0fe5db0872b7a0f36736633d813c178e385fba42
Ernest Frimpong Opoku <ernestfrimpongopoku.992@outlook.com>
2025-07-24 06:14:14 +0000

    Fix: refactored removefromArray to use updated array for removals

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...itemsToRemove) {
+  
+    let newArr = [...new Set(arr)];
+
+    for(let i = 0; i < itemsToRemove.length; i++){
+        const itemIndex = newArr.indexOf(itemsToRemove[i]);
+        
+        if (itemIndex !== -1){
+            newArr.splice(itemIndex, 1);
+        }
+    }
+
+    return newArr;
 };
+
+console.log(removeFromArray(["hey", 2, 3, "ho"], "hey", 3))
 
 // Do not edit below this line
 module.exports = removeFromArray;
