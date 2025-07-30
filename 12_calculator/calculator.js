6a986223afeb5e5f4149efadf6d9538c5029ffc9
Ernest Frimpong Opoku <ernestfrimpongopoku.992@outlook.com>
2025-07-30 18:03:28 +0000

    Fix: add return values to calculator  - add returns addition of two numbers  - subtract returns subtraction of two numbers  - sum returns addition multiple numbers  - multiply returns product of one or more numbers  - power returns exponents  - factorial returns the factorial of a number

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((acc, item) => acc + item, 0);
 };
 
-const multiply = function() {
+const multiply = function(arr) {
+  let result = 1;
 
+  for (let i = 0; i < arr.length; i++){
+    result *= arr[i];
+  }
+
+  return result;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	if (n === 0 || n === 1) return 1;
+
+  let result = 1;
+  for (let i = 2; i <= n; i++){
+    result *= i;
+  }
+
+  return result;
 };
 
 // Do not edit below this line
